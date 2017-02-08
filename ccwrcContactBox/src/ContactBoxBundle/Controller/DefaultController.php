42475226ef84d5ae71f74e12c8fa09cd061b1ed5
ccwrc <j.muszelik@gmail.com>
2017-02-08 17:02:06 +0100

    add contactbox bundle

---
+++ ccwrcContactBox/src/ContactBoxBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ContactBoxBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('ContactBoxBundle:Default:index.html.twig');
+    }
+}
