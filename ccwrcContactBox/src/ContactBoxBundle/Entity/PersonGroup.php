f03d21cbd7e3d6686cad7f4186c4ce8107ae8f6b
ccwrc <j.muszelik@gmail.com>
2017-02-11 11:40:47 +0100

    add new entity - persongroup

---
+++ ccwrcContactBox/src/ContactBoxBundle/Entity/PersonGroup.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace ContactBoxBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * PersonGroup
+ *
+ * @ORM\Table(name="person_group")
+ * @ORM\Entity(repositoryClass="ContactBoxBundle\Repository\PersonGroupRepository")
+ */
+class PersonGroup
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return PersonGroup
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
